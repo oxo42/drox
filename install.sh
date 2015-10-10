a811344247f30c23f8dd41e1d81e626ba70b2b8d
John Oxley <john.oxley@gmail.com>
2015-10-10 22:03:17 +0200

    Updated installer

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if ! [ -d ~/.drox ] ; then
-    git clone https://github.com/oxo42/drox.git ~/.drox
+    git clone --quiet https://github.com/oxo42/drox.git ~/.drox
+else
+    git pull -f
 fi
 cd ~/.drox
 ./update.sh
