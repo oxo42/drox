e1a105e84276ef38cb77a290c7b856a9741516df
John Oxley <john.oxley@gmail.com>
2015-10-12 10:31:26 +0100

    Update install

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 if ! [ -d ~/.drox ] ; then
+    # Clone from HTTP so we don't need auth
     git clone https://github.com/oxo42/drox.git ~/.drox
+    # Set push to SSH so I can push with my ssh-key that's carried in from login
+    git remote set-url --push origin git@github.com:oxo42/drox.git
+    cd ~/.drox
 else
+    cd ~/.drox
     git pull --force
 fi
-cd ~/.drox
 ./update.sh
-
