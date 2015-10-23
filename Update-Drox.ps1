fafcd7865d82c6d7d86cc2c97aef9792a666e899
John Oxley <john.oxley@liquidtelecom.com>
2015-10-23 17:19:07 +0100

    update update.ps1

--- Update-Drox.ps1
+++ Update-Drox.ps1
@@ -1,7 +1,19 @@
-function Test-ReparsePoint([string]$path) {
-    $file = Get-Item $path -Force -ea 0
-    return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+function Folder-Exists($path)
+{
+	return Test-Path -PathType Container -Path $path
 }
+
+function Folder-IsLink($path)
+{
+	$folder = Get-Item $path -Force
+	return [bool]($folder.Attributes -band [System.IO.FileAttributes]::ReparsePoint)
+}
+
+function Folder-IsSetup($path)
+{
+	return $(Folder-Exists($path)) -and $(Folder-IsLink($path))
+}
+
 
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
@@ -11,8 +23,10 @@ git pull --force
 echo "Check git submodule"
 git submodule update --recursive --init
 
-if(!(Test-ReparsePoint($HOME/.vim))) {
+if(!(Folder-IsSetup($HOME/.vim))) {
     echo mklink .vim
+} else {
+    echo .vim all good
 }
 
 echo "Finished at $(date)"
