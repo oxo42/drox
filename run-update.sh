3326f88820df44f6be7de6864bb9f0e82839933b
John Oxley <john.oxley@gmail.com>
2015-11-03 13:49:25 +0000

    Revert to .vim as it's own repo

--- run-update.sh
+++ run-update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 
+# This is because I used to have vim included in this repo with submodules 
+# but it now lives in it's own repo.  The git pull wouldn't clean out the 
+# submodules
+if [[ -d vim ]] ; then 
+    echo "Cleaning up old vim config"
+    rm -rf vim
+fi
+
 echo "Checking vim config"
 # Make ~/.vimrc source ~/.vim/vimrc
 if [[ -f ~/.vimrc && ! -L ~/.vimrc ]] ; then 
@@ -12,13 +20,24 @@ if [[ ! -L ~/.vimrc ]] ; then
     ln -s ~/.drox/dot.vimrc ~/.vimrc
 fi
 
-if [[ -d ~/.vim  && ! -L ~/.vim ]] ; then 
-    echo Backing up .vim
-    mv ~/.vim ~/.vim.bak  
+# Make sure that ~/.vim is a git repo and fully up to date
+if [[ -L ~/.vim ]] ; then 
+	rm ~/.vim
 fi
-if ! [[ -d ~/.vim && -L ~/.vim ]] ; then
-    echo Symlinking .vim
-    ln -s ~/.drox/vim ~/.vim
+if [[ -d ~/.vim ]] ; then
+    cd ~/.vim
+    if [[ $(git status 2> /dev/null) ]] ; then 
+        git pull
+        git submodule update --recursive --init
+    else
+        mv ~/.vim ~/.vim.bak
+    fi
+fi
+if [[ ! -d ~/.vim ]] ; then
+    git clone https://github.com/oxo42/vimrc.git ~/.vim
+    cd ~/.vim
+    git remote set-url --push origin git@github.com:oxo42/vimrc.git
+    git submodule update --recursive --init
 fi
 
 echo "Checking git config"
