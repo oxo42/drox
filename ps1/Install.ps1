c0b00ef92c307a5b47007776983296f0adb6ed25
John Oxley <john.oxley@gmail.com>
2016-04-12 13:11:08 +0100

    Updated install & update

--- ps1/Install.ps1
+++ ps1/Install.ps1
@@ -4,3 +4,6 @@ Set-PSRepository  -Name PSGallery
 Install-Module posh-git -Scope CurrentUser
 Install-Module PSReadline -Scope CurrentUser
 Install-Module Jump.Location -Scope CurrentUser
+
+# as Admin
+# cmd /c mklink .gitconfig .\drox\gitconfig
---
+++ ps1/Update.ps1
@@ -0,0 +1,6 @@
+cd ~/drox
+git pull
+
+cd ~/.vim
+git pull
+git submodule update --recusive --init
