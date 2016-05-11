303ee3e4565fab12753efeb6ab3a2c4b9b137b27
John Oxley <john.oxley@gmail.com>
2016-05-11 10:59:01 +0200

    Update standalone.sh

--- standaloneinstall.sh
+++ standaloneinstall.sh
@@ -2,3 +2,5 @@
 
 echo source ~/.drox/bashrc >> ~/.bashrc
 echo '(cd ~/.drox && ./update.sh 2>&1 &) >> /tmp/droxlog' >> ~/.profile
+
+curl https://raw.githubusercontent.com/oxo42/drox/master/install.sh | bash
